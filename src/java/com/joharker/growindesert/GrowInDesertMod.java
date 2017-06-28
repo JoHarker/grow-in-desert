f4ec7a784c5adf2dc264e5a7f40c176f6383139a
Igor Vilhovy <ukrainian12@gmail.com>
2017-06-28 03:38:25 +0300

    v1.12-0.1 The first commit of "Grow in desert" minecraft modification.
    Version 0.1 provides: using bone meal on dead bush to transform it to
    the acacia sapling. After that sand under the bush replacing by dirt
    block.

---
+++ src/java/com/joharker/growindesert/GrowInDesertMod.java
@@ -0,0 +1,10 @@
+package com.jharker.growindesert;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = GrowInDesertMod.MODID, version = GrowInDesertMod.VERSION)
+public class GrowInDesertMod
+{
+	public static final String MODID = "growindesert";
+	public static final String VERSION = "0.1";
+}
---
+++ src/java/com/joharker/growindesert/common/BonemealEventHandler.java
@@ -0,0 +1,39 @@
+package com.jharker.growindesert.common;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
+import net.minecraftforge.event.entity.player.BonemealEvent;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.block.BlockPlanks.EnumType;
+import net.minecraft.block.BlockSapling;
+import net.minecraft.block.state.IBlockState;
+
+@Mod.EventBusSubscriber
+public class BonemealEventHandler
+{
+	@SubscribeEvent
+	public static void BonemealEvent(BonemealEvent event)
+	{
+		if (event.getWorld().isRemote == false)
+		{
+			if (event.getBlock().getBlock() == Blocks.DEADBUSH)
+			{
+				IBlockState dirt = Blocks.DIRT.getDefaultState();
+				IBlockState sapling = Blocks.SAPLING.getDefaultState().withProperty(BlockSapling.TYPE, EnumType.ACACIA);
+
+				BlockPos under = event.getPos().add(0, -1.0, 0);
+
+				if (event.getWorld().getBlockState(under).getBlock() == Blocks.SAND)
+				{
+					event.getWorld().setBlockState(under, dirt);
+				}
+
+				event.getWorld().setBlockState(event.getPos(), sapling);
+
+				event.setResult(Result.ALLOW);
+			} 
+		}
+	}
+}
