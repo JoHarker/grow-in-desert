7c90f43c025a6ac408671801b59cb09f07c88f64
Igor Vilhovy <ukrainian12@gmail.com>
2017-07-01 00:29:20 +0300

    Water blocks increase probability, different plants may grows up, etc.
    Dead bush may transform not only to acacia. Support grass and fern.
    Water blocks in area of 9x9x2 with dead bush in center increase
    probability to growing will takes effect. Using square root function to
    better smooth of relation - number of water blocks ~ chance to growing.
    Water also increase chance of getting acacia (using linear function).
    Now, block under dead bush is replaced by grass block.

--- src/main/java/joharker/growindesert/GrowInDesertMod.java
+++ src/main/java/joharker/growindesert/GrowInDesertMod.java
@@ -6,7 +6,20 @@ import net.minecraftforge.fml.common.Mod;
 public class GrowInDesertMod
 {
 	public static final String MODID = "growindesert";
-	public static final String VERSION = "0.4";
-	
-	public static final float CHANCE_TO_GROWING = 0.05f;
+	public static final String VERSION = "0.3";
+
+	public static final float CHANCE_TO_GROWING;// Chance that bone meal will take effect
+	public static final float CHANCE_INCREMENT_WITH_WATER__GROWING;
+	static
+	{
+		CHANCE_TO_GROWING = 0.10f;
+
+		final int n = 10;// Number of blocks needed to absolute chance
+		CHANCE_INCREMENT_WITH_WATER__GROWING = // Using square root function
+				// to better smooth of relation - number of water blocks to chance
+				(float) ((1.0f - CHANCE_TO_GROWING) / Math.sqrt((double) n));
+	}
+
+	public static final float CHANCE_TO_GROWING_SAPLING = 0.3f;// Chance that result will be sapling
+	public static final float CHANCE_INCREMENT_WITH_WATER__SAPLING = 0.1f;// Linear increment
 }
--- src/main/java/joharker/growindesert/common/BonemealEventHandler.java
+++ src/main/java/joharker/growindesert/common/BonemealEventHandler.java
@@ -9,8 +9,9 @@ import net.minecraft.util.math.BlockPos;
 
 import java.util.Random;
 
-import net.minecraft.block.BlockPlanks.EnumType;
+import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.BlockSapling;
+import net.minecraft.block.BlockTallGrass;
 import net.minecraft.block.state.IBlockState;
 
 import joharker.growindesert.GrowInDesertMod;
@@ -25,24 +26,76 @@ public class BonemealEventHandler
 		{
 			if (event.getBlock().getBlock() == Blocks.DEADBUSH)
 			{
-				Random r = new Random();
-				if (r.nextFloat() < GrowInDesertMod.CHANCE_TO_GROWING)
+				float growingChanceIncrease;
+				float saplingChanceIncrease;
+				// To count water source blocks in area
 				{
-					IBlockState dirt = Blocks.DIRT.getDefaultState();
-					IBlockState sapling = Blocks.SAPLING.getDefaultState().withProperty(BlockSapling.TYPE,
-							EnumType.ACACIA);
-
-					BlockPos under = event.getPos().add(0, -1.0, 0);
-
-					if (event.getWorld().getBlockState(under).getBlock() == Blocks.SAND)
+					final int AREA_WIDTH = 9;
+					final int AREA_HEIGHT = 9;
+					int countBlocks = 0;
+					BlockPos pos = // move origin to left upper corner
+							event.getPos().add(-AREA_WIDTH / 2, 0, -AREA_HEIGHT / 2);
+					for (int y = 0; y < 2; y++, pos = pos.add(0, -1, 0))
 					{
-						event.getWorld().setBlockState(under, dirt);
+						for (int x = 0; x < AREA_WIDTH; x++, pos = pos.add(1, 0, 0))
+						{
+							for (int z = 0; z < AREA_HEIGHT; z++, pos = pos.add(0, 0, 1))
+							{
+								if (event.getWorld().getBlockState(pos).getBlock() == Blocks.WATER)
+								{
+									countBlocks++;
+									System.out.println(pos.getX() + " " + pos.getY() + " " + pos.getZ());
+								}
+							}
+							pos = pos.add(0, 0, -AREA_HEIGHT);
+						}
+						pos = pos.add(-AREA_WIDTH, 0, 0);
 					}
 
-					event.getWorld().setBlockState(event.getPos(), sapling);
+					// Calculate probability increase depending on water blocks count
+
+					growingChanceIncrease =
+							// Using square root function to better smooth
+							// of relation - number of water blocks to chance
+							(float) (GrowInDesertMod.CHANCE_INCREMENT_WITH_WATER__GROWING * Math.sqrt(countBlocks));
+
+					saplingChanceIncrease = // Using linear function. One block add constant value
+							GrowInDesertMod.CHANCE_INCREMENT_WITH_WATER__SAPLING * countBlocks;
 				}
+
+				Random r = new Random();
+				// Growing random check
+				if (r.nextFloat() < GrowInDesertMod.CHANCE_TO_GROWING + growingChanceIncrease)
+				{
+					IBlockState plant;
+					float saplingChance = GrowInDesertMod.CHANCE_TO_GROWING_SAPLING + saplingChanceIncrease;
+					float grassChance = (1.0f - saplingChance) / 2.0f;
+					// fernChance = remain probability
+					// Plants random check
+					float randomCheck = r.nextFloat();
+					if (randomCheck < saplingChance)
+					{// choose sapling
+						plant = Blocks.SAPLING.getDefaultState().withProperty(BlockSapling.TYPE,
+								BlockPlanks.EnumType.ACACIA);
+					} else if (randomCheck < saplingChance + grassChance)
+					{// choose grass
+						plant = Blocks.TALLGRASS.getDefaultState().withProperty(BlockTallGrass.TYPE,
+								BlockTallGrass.EnumType.GRASS);
+					} else
+					{// choose fern
+						plant = Blocks.TALLGRASS.getDefaultState().withProperty(BlockTallGrass.TYPE,
+								BlockTallGrass.EnumType.FERN);
+					}
+
+					BlockPos under = event.getPos().add(0, -1, 0);
+					event.getWorld().setBlockState(under, Blocks.GRASS.getDefaultState());
+					event.getWorld().setBlockState(event.getPos(), plant);
+				}
+
+				// takes one bone meal from player
 				event.setResult(Result.ALLOW);
-			}
+
+			} // end of 'if dead bush'
 		}
 	}
 }
